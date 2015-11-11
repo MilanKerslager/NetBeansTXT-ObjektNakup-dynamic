355c58e2d593f231cb6081d70824e915ac6908fc
ke <ke@B110-17.pslib.cz>
2015-11-11 10:24:05 +0100

    final version (dynamic)

--- src/nakup/Nakup.java
+++ src/nakup/Nakup.java
@@ -1,3 +1,7 @@
+// Načti nákup do seznamu (ArrayList).
+// Každá položka bude mít název, cenu za kus, počet kusů (budou to objekty).
+// Vypiš seznam nakoupených věcí.
+
 package nakup;
 
 import java.util.ArrayList;
@@ -7,29 +11,40 @@ public class Nakup {
 
     public static void main(String[] args) {
         PolozkaNakupu polozka;
-        polozka = new PolozkaNakupu();
         ArrayList<PolozkaNakupu> seznam = new ArrayList<>();
-        
         Scanner vstup = new Scanner(System.in);
         
+        polozka = new PolozkaNakupu();
         System.out.print("Vlož název: ");
         polozka.nazev = vstup.nextLine();
-        System.out.print("Vlož cenu za kus: ");
-        polozka.cenakus = vstup.nextDouble();
-        System.out.print("Vlož počet kusů: ");
-        polozka.kusu = vstup.nextInt();
+        while (! polozka.nazev.isEmpty()) {
+            System.out.print("Vlož cenu za kus: ");
+            polozka.cenakus = vstup.nextDouble();
+            System.out.print("Vlož počet kusů: ");
+            polozka.kusu = vstup.nextInt();
+            // odkaz na nový a naplněný objekt je vložen do seznamu
+            seznam.add(polozka);
+            // začneme načítat další objekt
+            vstup.nextLine();
+            // musíme si vytvořit nový objekt, abychom nepřepsali původní
+            // starý objekt nezanikne, protože je na něj odkaz z ArrayListu
+            polozka = new PolozkaNakupu();
+            System.out.print("Vlož název: ");
+            polozka.nazev = vstup.nextLine();
+        }
+        // poslední vytvořený objekt už nebudeme potřebovat
+        // (o uvolnění paměti se postará garbage collector)
+        polozka = null;
         
-        seznam.add(polozka);
-
         System.out.println();
+        System.out.println("Výpis položek (objektů) z ArrayListu:");
         
+        // vypíšeme celý seznam objektů uložený v ArrayListu
         for (PolozkaNakupu i: seznam) {
             System.out.println("Název zboží: "+i.nazev);
             System.out.println("Cena za kus: "+i.cenakus);
             System.out.println("Počet kusů: "+i.kusu);
-            System.out.println("Celkem za položku: "+
-                    i.cenakus*i.kusu);
+            System.out.println("Celkem za položku: "+i.cenakus*i.kusu);
         }
-    }
-    
+    }    
 }
--- src/nakup/PolozkaNakupu.java
+++ src/nakup/PolozkaNakupu.java
@@ -1,6 +1,17 @@
+// Třída pro vytváření objektů (s hodnotami - atributy).
+// Jednotlivé objekty vytvořené z této třídy 
+// jsou pak ukládány do seznamu (ArrayList).
+
 package nakup;
 
+// Třída nemá konstruktor ani gettery/settery.
+// Přístup k datům objektu je přímý (přes tečku).
+// Proto jsou atributy deklarovány jako public.
+// Správněji by měly být místo přímého přístupu použity
+// pro přístup k datům metody třídy (setter/getter).
+
 public class PolozkaNakupu {
+    // atributy objektu (členské proměnné)
     public String nazev;
     public double cenakus;
     public int kusu;
